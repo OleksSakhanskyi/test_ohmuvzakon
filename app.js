3b8ccf56494aaf67f5e31d036144382fd440eb00
Sakhanskyi Oleksandr <L20220126@SSPTAJI.CZ>
2024-10-21 12:20:14 +0200

    up

--- app.js
+++ app.js
@@ -14,13 +14,20 @@ function calculate() {
     let number1 = parseFloat(document.getElementById('number1').value);
     let number2 = parseFloat(document.getElementById('number2').value);
     let number3 = parseFloat(document.getElementById('number3').value);
-
+if((isNaN(number1)||isNaN(number2))&&(isNaN(number1)||isNaN(number3))&&(isNaN(number3)||isNaN(number2)))
+{
+    document.getElementById('text').innerText = "Nelze spocitat";
+     return;
+}
+else{
     if (isNaN(number1)) {
         number1 = number3 * number2;
         document.getElementById('number1').value = number1;
+        document.getElementById('text').innerText = "";
     } else if (isNaN(number2)) {
         number2 = number3 * number1;
         document.getElementById('number2').value = number2;
+        document.getElementById('text').innerText = "";
     } else if (isNaN(number3)) {
         if (number2 === 0) {
             document.getElementById('text').innerText = "Nelze dělit nulou";
@@ -28,7 +35,8 @@ function calculate() {
         } else {
             number3 = number1 / number2;
             document.getElementById('number3').value = number3;
+            document.getElementById('text').innerText = "";
         }
     }
 }
-    
+}
